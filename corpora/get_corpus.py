20f1046829b0fa5064470403e0736af441f46daf
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2020-09-27 22:28:52 -0700

    get_corpus

--- corpora/get_corpus.py
+++ corpora/get_corpus.py
@@ -3,32 +3,43 @@ import sys
 from urllib.request import urlopen
 from zipfile import ZipFile
 
-corpus_name = 'sttng'
+corpus_name = 'sttng6'
+
+
+def get_zip(url, path):
+    zipurl = 'Valid URL to zip file'
+        # Download the file from the URL
+    zipresp = urlopen(zipurl)
+        # Create a new file on the hard drive
+    tempzip = open("/tmp/tempfile.zip", "wb")
+        # Write the contents of the downloaded file into the new file
+    tempzip.write(zipresp.read())
+        # Close the newly-created file
+    tempzip.close()
+        # Re-open the newly-created file with ZipFile()
+    zf = ZipFile("/tmp/tempfile.zip")
+        # Extract its contents into <extraction_path>
+        # note that extractall will automatically create the path
+    zf.extractall(path = '<extraction_path>')
+        # close the ZipFile instance
+    zf.close()
+
 
 class Corpus():
-    def __init__(self, name=''):
+    def __init__(self, name='', url=''):
         self.name = name
+        self.url = url
+
+corpus_dict = {}
+corpus_dict['sttng6'] = Corpus('Star Trek: The Next Generation', r'https://github.com/jtquisenberry/NLP/raw/master/corpora/sttng6.zip')
 
 
+corpus_path = r'./{0}'.format(corpus_name)
 
-os.path.exists(r'./{0}'.format(corpus_name))
-corpus = Corpus('Star Trek: The Next Generation')
 
-zipurl = 'Valid URL to zip file'
-    # Download the file from the URL
-zipresp = urlopen(zipurl)
-    # Create a new file on the hard drive
-tempzip = open("/tmp/tempfile.zip", "wb")
-    # Write the contents of the downloaded file into the new file
-tempzip.write(zipresp.read())
-    # Close the newly-created file
-tempzip.close()
-    # Re-open the newly-created file with ZipFile()
-zf = ZipFile("/tmp/tempfile.zip")
-    # Extract its contents into <extraction_path>
-    # note that extractall will automatically create the path
-zf.extractall(path = '<extraction_path>')
-    # close the ZipFile instance
-zf.close()
-
+if os.path.exists(corpus_path):
+    print("CORPUS EXISTS")
+else:
+    corpus = corpus_dict['sttng6']
+    get_zip(corpus.url, corpus_path)
 
