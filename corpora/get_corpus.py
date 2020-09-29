8b0e00794a7ac6a9ed075ef12619f2c8b5331db1
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2020-09-28 22:41:42 -0700

    working implementation

--- corpora/get_corpus.py
+++ corpora/get_corpus.py
@@ -3,43 +3,70 @@ import sys
 from urllib.request import urlopen
 from zipfile import ZipFile
 
-corpus_name = 'sttng6'
+CORPUS = 'sttng6'
+
+if not os.path.exists(r'./data'):
+    os.mkdir(r'./data')
+
+a = 1
 
 
-def get_zip(url, path):
-    zipurl = 'Valid URL to zip file'
-        # Download the file from the URL
-    zipresp = urlopen(zipurl)
-        # Create a new file on the hard drive
-    tempzip = open("/tmp/tempfile.zip", "wb")
-        # Write the contents of the downloaded file into the new file
+def get_zip(url, path, zip_name):
+    zipresp = urlopen(url)
+    if not os.path.exists(r'./zips'):
+        os.mkdir(r'./zips')
+    if not os.path.exists(os.path.join(r'./zips', os.path.basename(path))):
+        os.mkdir(os.path.join(r'./zips', os.path.basename(path)))
+    zip_path = os.path.join(r'./zips', os.path.basename(path), zip_name)
+    tempzip = open(zip_path, 'wb')
     tempzip.write(zipresp.read())
-        # Close the newly-created file
     tempzip.close()
-        # Re-open the newly-created file with ZipFile()
-    zf = ZipFile("/tmp/tempfile.zip")
-        # Extract its contents into <extraction_path>
-        # note that extractall will automatically create the path
-    zf.extractall(path = '<extraction_path>')
-        # close the ZipFile instance
+    zf = ZipFile(zip_path)
+    zf.extractall(path=path)
     zf.close()
 
 
 class Corpus():
-    def __init__(self, name='', url=''):
+    def __init__(self, name='', url='', description='', subdirectory='', first_file=''):
         self.name = name
         self.url = url
+        self.description = description
+        self.subdirectory = subdirectory
+        self.first_file = first_file
 
 corpus_dict = {}
-corpus_dict['sttng6'] = Corpus('Star Trek: The Next Generation', r'https://github.com/jtquisenberry/NLP/raw/master/corpora/sttng6.zip')
+corpus_dict['sttng6'] = \
+    Corpus(name='Star Trek: The Next Generation',
+           url=r'https://github.com/jtquisenberry/NLP/raw/master/corpora/sttng6.zip',
+           description="All scripts of Star Trek: The Next Generation, preprocessed by ' + '\n' +"
+           "(1) concatenating scripts, (2) conversion on lowercase.",
+           subdirectory='sttng6',
+           first_file='sttng6.txt')
+corpus_dict['spampot_hpsl_concat'] = \
+    Corpus(name='Spampot',
+           url=r'https://github.com/jtquisenberry/NLP/raw/master/corpora/spampot_hpsl_concat.zip',
+           description='Email spam collection',
+           subdirectory='spampot_hpsl_concat',
+           first_file='spampot_hpsl_concat.txt')
+corpus_dict['princess_bride_orig'] =  \
+    Corpus(name='The Princess Bride Script Original',
+           url=r'https://github.com/jtquisenberry/NLP/raw/master/corpora/princess_bride_orig.zip',
+           description='The Princess Bride Script without text preprocessing',
+           subdirectory='princess_bride_orig',
+           first_file='princess_bride.txt')
 
+corpus = corpus_dict[CORPUS]
+download_directory = os.path.join(r'./data', corpus.subdirectory)
+zip_name = os.path.basename(corpus.url)
 
-corpus_path = r'./{0}'.format(corpus_name)
-
-
-if os.path.exists(corpus_path):
-    print("CORPUS EXISTS")
+if os.path.exists(download_directory):
+    if os.path.exists(os.path.join(download_directory, corpus.first_file)):
+        print("CORPUS EXISTS")
+    else:
+        get_zip(corpus.url, download_directory, zip_name)
 else:
-    corpus = corpus_dict['sttng6']
-    get_zip(corpus.url, corpus_path)
+    os.mkdir(download_directory)
+    get_zip(corpus.url, download_directory, zip_name)
+
+print('DONE')
 
